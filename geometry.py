Model looking good, issue with PBC after fixing distance formula

geometry.py
@@ -6,13 +6,19 @@ def __vertex_boundary_check(v1, v2, data):
     """
     Takes 2 vertices and returns a list vertex of the second vertex if it
     needs adjustment
+
+    
+    vvvvvvvvvvvvv
+    Used Strictly for polygon!!!
+    ^^^^^^^^^^^^^^^^^^
+
     """
     next_vertex = [v2[0], v2[1]]
-
+    
     if abs(v1[0] - v2[0]) > data.lx / 2:
-        next_vertex[0] -= data.lx
+        next_vertex[0] = abs(next_vertex[0] - data.lx)
     if abs(v1[1] - v2[1]) > data.ly / 2:
-        next_vertex[1] -= data.ly
+        next_vertex[1] = abs(next_vertex[1] - data.ly)
 
     return next_vertex
 
@@ -31,22 +37,37 @@ def create_polygon(vert_list, data):
     
     return vertices
 
+def __return_second_vertex(v1, v2, data):
+    next_vertex = [v2[0], v2[1]]
+    # check if either boundary is broken
+    if abs(v1[0] - v2[0]) > data.lx / 2:
+        if v1[0] > v2[0]:
+            next_vertex[0] = v2[0] + data.lx
+        else:
+            next_vertex[0] = v2[0] - data.lx
+    if abs(v1[1] - v2[1]) > data.ly / 2:
+        if v1[1] > v2[1]:
+            next_vertex[1] = v2[1] + data.lx
+        else:
+            next_vertex[1] = v2[1] - data.lx
+    return next_vertex
+
 def distance_formula(v1, v2):
     return ((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2 ) ** (1 / 2)
 
 def distance_formula_boundary_check(v1, v2, data):
-    v2 = __vertex_boundary_check(v1, v2, data)
+    v2 = __return_second_vertex(v1, v2, data)
     return distance_formula(v1, v2)
 
 def midpoint_formula(v1, v2):
     return np.array([(v1[0] + v2[0]) / 2, (v1[1] + v2[1]) / 2])
 
 def midpoint_formula_boundary_check(v1, v2, data):
-    v2 = __vertex_boundary_check(v1, v2, data)
+    v2 = __return_second_vertex(v1, v2, data)
     return midpoint_formula(v1, v2) % np.array([data.lx, data.ly])
 
 def unit_vector_boundary_check(v1, v2, data):
-    v2 = __vertex_boundary_check(v1, v2, data)
+    v2 = __return_second_vertex(v1, v2, data)
     vector = np.array([v2[0] - v1[0], v2[1] - v1[1]])
     magnitude = (vector ** 2).sum() ** (1 / 2)
     return vector / magnitude